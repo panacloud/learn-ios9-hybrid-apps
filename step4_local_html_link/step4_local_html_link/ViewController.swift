aaf453af23fd7c1fc8e4043ef218c951f67ffccf
Zia Khan <ziaukhan@hotmail.com>
2015-07-30 10:34:49 +0500

    completed step 4

---
+++ step4_local_html_link/step4_local_html_link/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  step4_local_html_link
+//
+//  Created by Zia Khan on 30/07/2015.
+//  Copyright © 2015 Panacloud. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController, WKNavigationDelegate {
+    
+    var webView: WKWebView?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let preferences = WKPreferences()
+        preferences.javaScriptEnabled = true
+        
+        let configuration = WKWebViewConfiguration()
+        configuration.preferences = preferences
+        
+        /* Now instantiate the web view */
+        webView = WKWebView(frame: view.bounds, configuration: configuration)
+        
+        if let theWebView = webView{
+            //Note that www is now a folder (blue) not a group (yellow), do you know the difference between the two?
+            let url = NSBundle.mainBundle().URLForResource("index", withExtension:"html", subdirectory: "www")
+            theWebView.loadFileURL(url!, allowingReadAccessToURL: url!)
+            
+            theWebView.navigationDelegate = self
+            view.addSubview(theWebView)
+            print("method executed")
+        }
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
